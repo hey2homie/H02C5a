8260acbf3f61a6eaeab015d98092f8f35622516d
hey2homie <photocard4@gmail.com>
2021-03-12 12:32:46 +0100

    Missing documentation. Should chechk condtion in isPassable

--- src/pacman/MazeMap.java
+++ src/pacman/MazeMap.java
@@ -1,33 +1,45 @@
 package pacman;
 
 /**
- * Each instance of this class represents a maze layout, specifying the width and height of the maze
- * and, for each position in the maze, whether it is passable or not.
+ * Each instance of this class represents a maze layout, specifying the width and height of the maze and, for each
+ * position in the maze, whether it is passable or not.
  */
 public class MazeMap {
+
+	public int width;
+	public int height;
+	public boolean[] passable;
 
 	/**
 	 * Returns the width (i.e. the number of columns) of this maze map.
 	 */
-	public int getWidth() { throw new RuntimeException("Not yet implemented"); }
-	
+	public int getWidth() {
+		return width;
+	}
+
 	/**
 	 * Returns the height (i.e. the number of rows) of this maze map.
 	 */
-	public int getHeight() { throw new RuntimeException("Not yet implemented"); }
+	public int getHeight() {
+		return height;
+	}
 	
 	/**
 	 * Returns whether the square in this maze at row index {@code row} and column index {@code column} is passable.
 	 * The square in the top-left corner of the maze has row index 0 and column index 0.
 	 */
-	public boolean isPassable(int rowIndex, int columnIndex) { throw new RuntimeException("Not yet implemented"); }
+	public boolean isPassable(int rowIndex, int columnIndex) {
+		return rowIndex == 0 & columnIndex == 0;	// Don't really know what condition should be here
+	}
 	
 	/**
 	 * Initializes this object so that it represents a maze layout with the given width, height, and
 	 * passable positions. The passable positions are given in row-major order (i.e. the first {@code width} elements
-	 * of {@code passable} specify the passability of the maze positions in the first row of the maze). 
+	 * of {@code passable} specify the possibility of the maze positions in the first row of the maze).
 	 */
 	public MazeMap(int width, int height, boolean[] passable) {
-		throw new RuntimeException("Not yet implemented");
+		this.width = width;
+		this.height = height;
+		this.passable = passable;
 	}
 }
