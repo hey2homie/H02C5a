fc7619acc1c78c9f1d13273e2b12a1211a66135a
hey2homie <photocard4@gmail.com>
2021-03-20 23:27:54 +0100

    Implemented methods. Missing test suites. Need to complete documentation

--- src/pacman/PacMan.java
+++ src/pacman/PacMan.java
@@ -2,22 +2,75 @@ package pacman;
 
 /**
  * Each instance of this class represents the player-controlled Pac-Man character in a Pac-Man maze.
+ *
+ * @mutable
  */
 public class PacMan {
-	
-	public Square getSquare() { throw new RuntimeException("Not yet implemented"); }
-	
-	public int getNbLives() { throw new RuntimeException("Not yet implemented"); }
 
-	public PacMan(int nbLives, Square square) {
-		throw new RuntimeException("Not yet implemented");
+	/**
+	 * @invar |
+	 * @invar |
+	 */
+	private int nbLives;
+	private Square square;
+
+	/**
+	 * Returns
+	 *
+	 * @basic
+	 */
+	public Square getSquare() {
+		return square;
 	}
-	
-	public void setSquare(Square square) { throw new RuntimeException("Not yet implemented"); }
-	
+
+	/**
+	 * Returns this object number of lives.
+	 *
+	 * @basic
+	 */
+	public int getNbLives() {
+		return nbLives;
+	}
+
+	/**
+	 *
+	 *
+	 * @pre | nbLives > 1
+	 * @pre | square != null
+	 *
+	 * @throws IllegalArgumentException if method's nbLives parameter is less then 1 or square is null.
+	 */
+	public PacMan(int nbLives, Square square) {
+		if (nbLives < 1 || square == null ) {
+			throw new IllegalArgumentException("Wrong parameters");
+		}
+
+		this.nbLives = nbLives;
+		this.square = square;
+	}
+
+	/**
+	 * Sets this object square.
+	 *
+	 * @mutates | this.square
+	 */
+	public void setSquare(Square square) {
+		if (square == null) {
+			throw new IllegalArgumentException("Square is null");
+		}
+
+		this.square = square;
+	}
+
 	/**
 	 * Decreases this Pac-Man character's number of lives by one.
+	 *
+	 * @mutates | this.nbLives
+	 *
+	 * @post | getNbLives() == getNbLives() - 1
 	 */
-	public void die() { throw new RuntimeException("Not yet implemented"); }
+	public void die() {
+		this.nbLives -= 1;
+	}
 
 }
