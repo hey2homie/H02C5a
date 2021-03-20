4073bd466d29bf31e799fca1d3e0240add670f0f
hey2homie <photocard4@gmail.com>
2021-03-20 23:04:51 +0100

    Implement methods of Dot class and write initial (?) documentation. Miss test suit

--- src/pacman/Dot.java
+++ src/pacman/Dot.java
@@ -2,11 +2,36 @@ package pacman;
 
 /**
  * Each instance of this class represents a dot (a piece of food for Pac-Man) in a Pac-Man maze.
+ *
+ * @immutable
+ *
+ * @invar | This
  */
 public class Dot {
-	
-	public Square getSquare() { throw new RuntimeException("Not yet implemented"); }
-	
-	public Dot(Square square) { throw new RuntimeException("Not yet implemented"); }
+
+	/**
+	 * @invar | square != null
+	 */
+	private final Square square;
+
+	/**
+	 * Returns object of Square class.
+	 *
+	 * @basic
+	 */
+	public Square getSquare() {
+		return square;
+	}
+
+	/**
+	 * Initializes this object so that it represents a dot at maze square.
+	 *
+	 * @pre | square != null
+	 *
+	 * @post | getSquare == square
+	 */
+	public Dot(Square square) {
+		this.square = square;
+	}
 
 }
