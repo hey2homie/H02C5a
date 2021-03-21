b311eb05b054b6f2b8e5da4165cdc88ff5e751c5
hey2homie <photocard4@gmail.com>
2021-03-21 01:41:23 +0100

    Add empty GhostTest test suite

---
+++ src/pacman/tests/GhostTest.java
@@ -0,0 +1,24 @@
+package pacman.tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GhostTest {
+
+    @Test
+    void getSquare() {
+    }
+
+    @Test
+    void getDirection() {
+    }
+
+    @Test
+    void setSquare() {
+    }
+
+    @Test
+    void setDirection() {
+    }
+}
