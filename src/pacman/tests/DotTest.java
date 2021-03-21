37462018b6e0ac825e76fb94358fadc898591334
hey2homie <photocard4@gmail.com>
2021-03-21 18:27:03 +0100

    Add test DotTest

--- src/pacman/tests/DotTest.java
+++ src/pacman/tests/DotTest.java
@@ -1,12 +1,27 @@
 package pacman.tests;
 
 import org.junit.jupiter.api.Test;
+import pacman.Dot;
+import pacman.MazeMap;
+import pacman.Square;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DotTest {
 
+    MazeMap map = new MazeMap(3, 3,
+            new boolean[] {
+                    false, true, true,
+                    true, true, true,
+                    true, false, true
+            }
+    );
+
+    Square square = new Square(map, 0, 0);
+    Dot dot = new Dot(square);
+
     @Test
     void getSquare() {
+        assertEquals(square, dot.getSquare());
     }
 }
