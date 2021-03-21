88c05c6fd94f751592fa296fafa4f78e8f3d9928
hey2homie <photocard4@gmail.com>
2021-03-21 01:37:48 +0100

    Add empty PacManTest test suite

---
+++ src/pacman/tests/PacManTest.java
@@ -0,0 +1,24 @@
+package pacman.tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PacManTest {
+
+    @Test
+    void getSquare() {
+    }
+
+    @Test
+    void getNbLives() {
+    }
+
+    @Test
+    void setSquare() {
+    }
+
+    @Test
+    void die() {
+    }
+}
