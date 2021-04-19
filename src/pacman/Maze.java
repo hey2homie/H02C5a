5e1a5b803a42e5989d1b1c69807d990b8df57418
hey2homie <photocard4@gmail.com>
2021-04-19 18:43:46 +0300

    PowerPellet is added to the Maze class. Needs further implementation

--- src/pacman/Maze.java
+++ src/pacman/Maze.java
@@ -3,21 +3,21 @@ package pacman;
 import java.util.Random;
 
 public class Maze {
-	
+
 	private final Random random;
 	private final MazeMap map;
 	private final PacMan pacMan;
 	private final Ghost[] ghosts;
 	private FoodItem[] foodItems;
-	
+
 	public MazeMap getMap() {
 		return map;
 	}
-	
+
 	public PacMan getPacMan() {
 		return pacMan;
 	}
-	
+
 	public Ghost[] getGhosts() {
 		return ghosts.clone();
 	}
@@ -25,7 +25,7 @@ public class Maze {
 	public FoodItem[] getFoodItems() {
 		return foodItems.clone();
 	}
-	
+
 	public Maze(Random random, MazeMap map, PacMan pacMan, Ghost[] ghosts, FoodItem[] foodItems) {
 		this.random = random;
 		this.map = map;
@@ -33,30 +33,30 @@ public class Maze {
 		this.ghosts = ghosts.clone();
 		this.foodItems = foodItems.clone();
 	}
-	
+
 	public boolean isCompleted() {
 		return foodItems.length == 0;
 	}
-	
+
 	private void checkPacManDamage() {
 		for (Ghost ghost : ghosts)
 			if (ghost.getSquare().equals(pacMan.getSquare()))
 				pacMan.die();
 	}
-	
+
 	public void moveGhosts() {
 		for (Ghost ghost : ghosts)
 			ghost.move(random);
 		checkPacManDamage();
 	}
-	
+
 	private void removeFoodItemAtIndex(int index) {
 		FoodItem[] newFoodItems = new FoodItem[foodItems.length - 1];
 		System.arraycopy(foodItems, 0, newFoodItems, 0, index);
 		System.arraycopy(foodItems, index + 1, newFoodItems, index, newFoodItems.length - index);
 		foodItems = newFoodItems;
 	}
-	
+
 	private void removeFoodItemAtSquare(Square square) {
 		for (int i = 0; i < foodItems.length; i++) {
 			if (foodItems[i].getSquare().equals(square)) {
@@ -65,14 +65,28 @@ public class Maze {
 			}
 		}
 	}
-	
+
+	// TODO: Move from instanceof to dynamic binding
+	private boolean isPowerPellet(Square square) {
+		for (FoodItem foodItem : foodItems) {
+			if (foodItem.getSquare().equals(square) && foodItem instanceof PowerPellet) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
 	public void movePacMan(Direction direction) {
 		Square newSquare = pacMan.getSquare().getNeighbor(direction);
 		if (newSquare.isPassable()) {
 			pacMan.setSquare(newSquare);
+			if (isPowerPellet(newSquare)) {
+				// TODO: Implement change of ghost state
+				throw new IllegalArgumentException("Not yet implemented");
+			}
 			removeFoodItemAtSquare(newSquare);
 			checkPacManDamage();
 		}
 	}
-	
 }
