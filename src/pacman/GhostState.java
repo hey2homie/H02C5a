b47dada2e277b98d6541488d6fb2df4603bd63be
hey2homie <photocard4@gmail.com>
2021-04-26 22:34:37 +0200

    Implementation of GhostState class.
    Need additional functionality in the move and all relevant to move methods.

--- src/pacman/GhostState.java
+++ src/pacman/GhostState.java
@@ -1,5 +1,30 @@
 package pacman;
 
-public class GhostState {
+import java.util.Random;
 
+public abstract class GhostState {
+
+    private int delay = 0;
+
+    public GhostState move(Ghost ghost, Random random) {
+        return null;
+    }
+
+    public void increaseDelay() {
+        this.delay++;
+    }
+
+    public void decreaseDelay() {
+        this.delay--;
+    }
+
+    public GhostState hitBy(Ghost ghost, PacMan pacMan) {
+        if (ghost.isVulnerable()) {
+            ghost.setSquare(ghost.getOriginalSquare());
+            return new RegularGhostState();
+        } else {
+            pacMan.die();
+            return new VulnerableGhostState();
+        }
+    }
 }
