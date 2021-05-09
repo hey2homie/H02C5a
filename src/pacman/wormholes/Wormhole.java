f603401f7c7e1118245c50d651bc9de15937c92f
hey2homie <photocard4@gmail.com>
2021-05-09 16:40:51 +0200

    Implementation of Wormholes.

--- src/pacman/wormholes/Wormhole.java
+++ src/pacman/wormholes/Wormhole.java
@@ -1,4 +1,34 @@
 package pacman.wormholes;
 
 public class Wormhole {
+
+    private ArrivalPortal arrivalPortal;
+    private DeparturePortal departurePortal;
+
+    public Wormhole(DeparturePortal departurePortal, ArrivalPortal arrivalPortal) {
+        if (departurePortal == null || arrivalPortal == null) {
+            throw new IllegalArgumentException("Wrong parameters");
+        }
+        this.arrivalPortal = arrivalPortal;
+        this.departurePortal = departurePortal;
+        this.departurePortal.join(this);
+    }
+
+    public ArrivalPortal getArrivalPortal() {
+        return arrivalPortal;
+    }
+
+    public DeparturePortal getDeparturePortal() {
+        return departurePortal;
+    }
+
+    public void setArrivalPortal(ArrivalPortal arrivalPortal) {
+        this.arrivalPortal = arrivalPortal;
+    }
+
+    public void setDeparturePortal(DeparturePortal departurePortal) {
+        this.departurePortal.remove(this);
+        this.departurePortal = departurePortal;
+        this.departurePortal.join(this);
+    }
 }
